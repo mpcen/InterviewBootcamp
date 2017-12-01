af006661500cc1d5c2044980a530a30e7504f232
mpcen <vccengineers12@gmail.com>
2017-11-30 23:48:23 -0800

    added iterative fibonacci solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,24 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// iterative
+function fib(n) {
+	if(n === 0) {
+		return 0
+	} else if(n === 1) {
+		return 1
+	} else {
+		const entries = [0, 1];
+
+		for(let i = 2; i <= n; i++) {
+			const a = entries[i - 1];
+			const b = entries[i - 2];
+			entries.push(a + b);
+		}
+
+		return entries[entries.length - 1];
+	}
+
+}
 
 module.exports = fib;
