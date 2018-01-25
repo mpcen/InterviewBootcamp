6654a1adddf39db8e04cebff6906f105bda349c6
mpcen <vccengineers12@gmail.com>
2018-01-25 02:06:30 -0800

    added bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -3,10 +3,14 @@
 // a binary search tree.  The constructor
 // should initialize values 'data', 'left',
 // and 'right'.
+
+
 // 2) Implement the 'insert' method for the
 // Node class.  Insert should accept an argument
 // 'data', then create an insert a new node
 // at the appropriate location in the tree.
+
+
 // 3) Implement the 'contains' method for the Node
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
@@ -19,21 +23,27 @@ class Node {
 	}
 
 	insert(data) {
-		if(data < this.data) {
-			if(this.left === null) {
-				return this.left = new Node(data);
-			}
+		if(data < this.data && this.left) {
+			this.left.insert(data);
+		} else if(data < this.data) {
+			this.left = new Node(data);
+		} else if(data > this.data && this.right) {
+			this.right.insert(data);
+		} else if(data > this.data) {
+			this.right = new Node(data);
+		}
+	}
 
-			return this.left.insert(data, this.left);
+	contains(data) {
+		if(data === this.data) return this;
+
+		if(data < this.data && this.left) {
+			return this.left.contains(data);
+		} else if(data > this.data && this.right) {
+			return this.right.contains(data);
 		}
 
-		if(data > this.data) {
-			if(this.right === null) {
-				return this.right = new Node(data);
-			}
-
-			return this.right.insert(data, this.right);
-		}
+		return null;
 	}
 }
 
